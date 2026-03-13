333d40d5b610f4bf865729f411d4bf0213a2604d
Luciano Scarabelli <luciano@scarcom.com.br>
2026-03-13 13:15:35 +0000

    Publish 3 from staging into origin/main

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -20,9 +20,6 @@ import * as $DetailsControl from "./islands/DetailsControl.tsx";
 import * as $ExchangeForm from "./islands/ExchangeForm.tsx";
 import * as $ExpandableDescription from "./islands/ExpandableDescription.tsx";
 import * as $Form from "./islands/Form.tsx";
-import * as $HeaderButton from "./islands/HeaderButton.tsx";
-import * as $HeaderModals from "./islands/HeaderModals.tsx";
-import * as $HeaderSearchbar from "./islands/HeaderSearchbar.tsx";
 import * as $Header_Buttons from "./islands/Header/Buttons.tsx";
 import * as $Header_Cart_linx from "./islands/Header/Cart/linx.tsx";
 import * as $Header_Cart_nuvemshop from "./islands/Header/Cart/nuvemshop.tsx";
@@ -30,6 +27,9 @@ import * as $Header_Cart_shopify from "./islands/Header/Cart/shopify.tsx";
 import * as $Header_Cart_vnda from "./islands/Header/Cart/vnda.tsx";
 import * as $Header_Cart_vtex from "./islands/Header/Cart/vtex.tsx";
 import * as $Header_Cart_wake from "./islands/Header/Cart/wake.tsx";
+import * as $HeaderButton from "./islands/HeaderButton.tsx";
+import * as $HeaderModals from "./islands/HeaderModals.tsx";
+import * as $HeaderSearchbar from "./islands/HeaderSearchbar.tsx";
 import * as $LgdpModal from "./islands/LgdpModal.tsx";
 import * as $LogComponent from "./islands/LogComponent.tsx";
 import * as $Newsletter from "./islands/Newsletter.tsx";
@@ -71,9 +71,6 @@ const manifest = {
     "./islands/ExchangeForm.tsx": $ExchangeForm,
     "./islands/ExpandableDescription.tsx": $ExpandableDescription,
     "./islands/Form.tsx": $Form,
-    "./islands/HeaderButton.tsx": $HeaderButton,
-    "./islands/HeaderModals.tsx": $HeaderModals,
-    "./islands/HeaderSearchbar.tsx": $HeaderSearchbar,
     "./islands/Header/Buttons.tsx": $Header_Buttons,
     "./islands/Header/Cart/linx.tsx": $Header_Cart_linx,
     "./islands/Header/Cart/nuvemshop.tsx": $Header_Cart_nuvemshop,
@@ -81,6 +78,9 @@ const manifest = {
     "./islands/Header/Cart/vnda.tsx": $Header_Cart_vnda,
     "./islands/Header/Cart/vtex.tsx": $Header_Cart_vtex,
     "./islands/Header/Cart/wake.tsx": $Header_Cart_wake,
+    "./islands/HeaderButton.tsx": $HeaderButton,
+    "./islands/HeaderModals.tsx": $HeaderModals,
+    "./islands/HeaderSearchbar.tsx": $HeaderSearchbar,
     "./islands/LgdpModal.tsx": $LgdpModal,
     "./islands/LogComponent.tsx": $LogComponent,
     "./islands/Newsletter.tsx": $Newsletter,
